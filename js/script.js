71a8072c27c5653749743d20608b269d8a2a1244
deathguuun <68237608+deathguuun@users.noreply.github.com>
2020-08-13 13:01:45 +0300

    Fix accordion js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,24 @@
-let coll = document.getElementsByClassName('question__title');
-for (let i = 0; i < coll.length; i++) {
-   coll[i].addEventListener('click', function () {
-      this.classList.toggle('active');
-      let content = this.nextElementSibling;
-      if (content.style.maxHeight) {
-         content.style.maxHeight = null;
-      } else {
-         content.style.maxHeight = content.scrollHeight + 'px'
-      }
-   })
-}
-
-$(".question__summary").click(function () {
-   $(this).toggleClass("clicked");
+// Accordion
+$(".question__title").on("click", function () {
+   if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+         .siblings(".question__content")
+         .slideUp(200);
+      $(this).find(".question__icon")
+         .removeClass("clicked")
+   } else {
+      $(this).find(".question__icon")
+         .removeClass("clicked")
+      $(this)
+         .find(".question__icon")
+         .addClass("clicked");
+      // $(".set > a").removeClass("active");
+      $(this).addClass("active");
+      // $(".content").slideUp(200);
+      $(this)
+         .siblings(".question__content")
+         .slideDown(200);
+   }
 });
 
-
-
-
-/* jQuery
-================================================== */
-$(function () {
-   $('.acc__title').click(function (this_element) {
-
-      var dropDown = $(this).closest('.acc__card').find('.acc__panel');
-      $(this).closest('.question__wrapper').find('.acc__panel').not(dropDown).slideUp();
-
-      if ($(this).hasClass('active')) {
-         $(this).removeClass('active');
-      } else {
-         $(this).closest('.question__wrapper').find('.question__title.active').removeClass('active');
-         $(this).addClass('active');
-      }
-
-      dropDown.stop(false, true).slideToggle();
-      this_element.preventDefault();
-   });
-});
