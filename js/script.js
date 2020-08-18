7cbd139c1a7bc9a4744f9a0bcf1994d6e4ce55fe
i-smuglov <iurii.smuglov@enlabs.se>
2020-08-18 11:23:59 +0300

    rename question to faq

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // Accordion
-$(".question__title").on("click", function () {
+$(".faq__title").on("click", function () {
    if ($(this).hasClass("active")) {
       $(this).removeClass("active");
       $(this)
-         .siblings(".question__content")
+         .siblings(".faq__content")
          .slideUp(200);
-      $(this).find(".question__icon")
+      $(this).find(".faq__icon")
          .removeClass("clicked")
    } else {
-      $(this).find(".question__icon")
+      $(this).find(".faq__icon")
          .removeClass("clicked")
       $(this)
-         .find(".question__icon")
+         .find(".faq__icon")
          .addClass("clicked");
       // $(".set > a").removeClass("active");
       $(this).addClass("active");
       // $(".content").slideUp(200);
       $(this)
-         .siblings(".question__content")
+         .siblings(".faq__content")
          .slideDown(200);
    }
 });
